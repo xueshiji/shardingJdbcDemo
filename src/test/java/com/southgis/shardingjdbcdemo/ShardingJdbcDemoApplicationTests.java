05e3b112f7313cb317a148bb890ee2c9fa52940b
xueshiji <shiji.xue@southgis.com>
2021-12-15 15:53:25 +0800

    commit

--- src/test/java/com/southgis/shardingjdbcdemo/ShardingJdbcDemoApplicationTests.java
+++ src/test/java/com/southgis/shardingjdbcdemo/ShardingJdbcDemoApplicationTests.java
@@ -13,16 +13,15 @@ class ShardingJdbcDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
-		orderMapper.insert(new orderEntity("0","name0",1.0));
-		orderMapper.insert(new orderEntity("1","name0",1.0));
-		orderMapper.insert(new orderEntity("2","name0",1.0));
-		orderMapper.insert(new orderEntity("3","name0",1.0));
+		orderMapper.insert(new orderEntity("0", "name", 1.0));
+		orderMapper.insert(new orderEntity("1", "name", 1.0));
+		orderMapper.insert(new orderEntity("2", "name", 1.0));
+		orderMapper.insert(new orderEntity("3", "name", 1.0));
 		List<orderEntity> orderEntities = orderMapper.selectList(null);
 		System.out.println(orderEntities);
 		System.out.println(orderMapper.deleteById("3"));
-		orderMapper.updateById(new orderEntity("3","name0",2.0));
-		orderEntities = orderMapper.selectList(null);
-		System.out.println(orderEntities);
+		orderMapper.updateById(new orderEntity("2", "name2", 2.0));
+		System.out.println(orderMapper.selectById(2));
 		orderMapper.delete(null);
 	}
 
